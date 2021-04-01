8bcc0e3ac68dc926ba7829d84da38d51b27cd863
Mats Lidell <mats.lidell@lidells.se>
2021-04-01 17:20:23 +0200

    Add button for running hyperbole ert tests

---
+++ hypb-ert.el
@@ -0,0 +1,45 @@
+;;; hypb-ert --- ert button support                   -*- lexical-binding: t; -*-
+
+;; Author: Mats Lidell <matsl@gnu.org>
+;;
+;; Orig-Date: 31-Mar-21 at 21:11:00
+;;
+;; Copyright (C) 2021  Free Software Foundation, Inc.
+;; See the "HY-COPY" file for license information.
+;;
+;; This file is part of GNU Hyperbole.
+
+;;; Commentary:
+
+;; Defines an implicit button for running an ert test
+;;
+;; Example:
+;;   Run the test hbut-defal-url
+;;   <hypb-ert-sym hbut-defal-url>
+;;
+;;   Run the tests specified by the test selector hbut-defal
+;;   <hypb-ert-sel hbut-defal>
+;;
+;;   Run all tests
+;;   <hypb-ert-sel t>
+
+;;; Code:
+
+(require 'hbut)
+(require 'hargs)
+
+(defun hypb-run-ert-test-symbol (test-symbol)
+  "Run the specified TEST-SYMBOL ert test."
+  (mapc 'load-file (directory-files "test" t "\\.el$"))
+  (ert (intern test-symbol)))
+
+(defun hypb-run-ert-test-selector (test-selector)
+  "Run the specified TEST-SELECTOR defined ert test."
+  (mapc 'load-file (directory-files "test" t "\\.el$"))
+  (ert test-selector))
+
+(defal hypb-ert-sym 'hypb-run-ert-test-symbol)
+(defal hypb-ert-sel 'hypb-run-ert-test-selector)
+
+(provide 'hypb-ert)
+;;; hypb-ert.el ends here
