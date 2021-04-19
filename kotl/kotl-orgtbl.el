84e1b11cf40128e3d5d786528bc036c8c47c0cba
Bob Weiner <rsw@gnu.org>
2021-04-19 00:45:20 -0400

    Update kotl-orgtbl.el with Copyright and doc

--- kotl/kotl-orgtbl.el
+++ kotl/kotl-orgtbl.el
@@ -1,8 +1,58 @@
-;;; Allow use of Org minor-mode table editing in Koutlines
-;;; If in an Org table, use kotl-mode {TAB} binding only when given an
-;;; explicit prefix argument; otherwise, {TAB} acts normally in tables.
+;;; kotl-orgtbl.el --- Allow use of Org minor-mode table editing in Koutlines
+;;
+;; Author:       Bob Weiner
+;;
+;; Orig-Date:    10/18/2020
+;;
+;; Copyright (C) 2020-2021  Free Software Foundation, Inc.
+;; See the "../HY-COPY" file for license information.
+;;
+;; This file is part of GNU Hyperbole.
+
+;;; Commentary:
+;;
+
+;;   - Org Table Support: Org table editing now automatically works in the
+;;     Koutliner via Org table minor mode.  Use {M-x orgtbl-mode RET} to
+;;     toggle this on and off or simply press the Action Key on a pipe |
+;;     character to do the same thing.  See "(Org)Tables" for details.
+
+;;   - New Tree Demotion/Promotion Keys: Tree promotion and demotion keys now
+;;     match the defaults in Org mode and Outline mode, plus some easier to
+;;     type ones.  The tables below summarize which keys work whether inside
+;;     an Org table or outside.
+
+;;     |----------------------------+-----------------------------|
+;;     | Demotion Inside Org Table  | Promotion Inside Org Table  |
+;;     |----------------------------+-----------------------------|
+;;     | C-c C-,                    | C-c C-.                     |
+;;     | C-c C-<                    | C-c C->                     |
+;;     |----------------------------+-----------------------------|
+
+;;     |----------------------------+-----------------------------|
+;;     | Demotion Outside Org Table | Promotion Outside Org Table |
+;;     |----------------------------+-----------------------------|
+;;     | TAB                        | Shift-TAB or M-TAB          |
+;;     | M-left-arrow               | M-right-arrow               |
+;;     | C-c C-,                    | C-c C-.                     |
+;;     | C-c C-<                    | C-c C->                     |
+;;     |----------------------------+-----------------------------|
+
+;;  If in an Org table, the kotl-mode {TAB} binding operates only
+;;  when invoked with an explicit prefix argument; otherwise, {TAB}
+;;  performs its normal table-based alignment and movement.
+
+;;; Code:
+
+;;; ************************************************************************
+;;; Other required Elisp libraries
+;;; ************************************************************************
 
 (require 'org-table)
+
+;;; ************************************************************************
+;;; Public functions
+;;; ************************************************************************
 
 ;; Redefine this Org Table function to handle Koutlines as well.
 (defun orgtbl-tab (arg)
@@ -32,11 +82,12 @@ If no previous line, exchange current with next line."
   )
 
 (defun orgtbl-meta-return (arg)
-  "Let Action Key handle tables in kotl-mode, otherwise, use standard
-Org table command."
+  "Let Action Key handle tables in kotl-mode, otherwise, use standard Org table command."
   (interactive "P")
   (if (derived-mode-p #'kotl-mode)
       (hkey-either arg)
     (org-table-wrap-region arg)))
 
 (provide 'kotl-orgtbl)
+
+;;; kotl-orgtbl.el ends here
