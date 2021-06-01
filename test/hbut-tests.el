ebfda095be2b051a4d16ae641ca299c8752acd98
Mats Lidell <mats.lidell@lidells.se>
2021-06-01 22:22:55 +0200

    Add for ebut shell cmd and hbut delete

--- test/hbut-tests.el
+++ test/hbut-tests.el
@@ -42,5 +42,32 @@
           (should (equal (hattr:get (hbut:at-p) 'lbl-key) "label")))
       (delete-file file))))
 
+(ert-deftest ebut-program-shell-cmd ()
+  "Programatically create ebut running shell command."
+  (let ((file (make-temp-file "hypb_" nil ".txt")))
+    (unwind-protect
+        (progn
+          (find-file file)
+          (ebut:program "label" 'exec-shell-cmd "ls /tmp")
+          (should (eq (hattr:get (hbut:at-p) 'actype) 'actypes::exec-shell-cmd))
+          (should (equal (hattr:get (hbut:at-p) 'args) '("ls /tmp")))
+          (should (equal (hattr:get (hbut:at-p) 'lbl-key) "label")))
+      (delete-file file))))
+
+(ert-deftest ebut-delete-removes-ebut-and-returns-button-data ()
+  "Remove a ebut."
+  (let ((file (make-temp-file "hypb_" nil ".txt")))
+    (unwind-protect
+        (progn
+          (find-file file)
+          (ebut:program "label" 'link-to-directory "/tmp")
+          (should (hbut:at-p))
+          (let ((args (hbut:delete)))
+            (should-not (hbut:at-p))
+            (should (equal (hbdata:actype args) "actypes::link-to-directory"))
+            (should (equal (hbdata:args args) '("/tmp")))
+            ))
+      (delete-file file))))
+
 (provide 'hbut-tests)
 ;;; hbut-tests.el ends here
