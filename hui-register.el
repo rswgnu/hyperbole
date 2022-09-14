21ef6f57b44c0deef4c46c95a2c8bd9a7e989214
Mats Lidell <mats.lidell@lidells.se>
2022-09-14 21:00:13 +0200

    Use name hui-register

--- hui-register.el
+++ hui-register.el
@@ -1,9 +1,9 @@
-;;; hypb-register.el --- register support for Hyperbole    -*- lexical-binding: t; -*-
+;;; hui-register.el --- register support for Hyperbole    -*- lexical-binding: t; -*-
 ;;
 ;; Author:       Mats Lidell
 ;;
 ;; Orig-Date:     6-Oct-91 at 03:42:38
-;; Last-Mod:     11-Sep-22 at 00:39:27 by Mats Lidell
+;; Last-Mod:     14-Sep-22 at 20:59:47 by Mats Lidell
 ;;
 ;; Copyright (C) 1991-2022  Free Software Foundation, Inc.
 ;; See the "HY-COPY" file for license information.
@@ -26,19 +26,19 @@
 ;;; Public functions
 ;;; ************************************************************************
 
-(cl-defstruct hypb-register-but
+(cl-defstruct hui-register-but
   "Button register struct."
   label file mpos link)
 
 ;;;###autoload
-(defun hypb-register-struct-at-point ()
+(defun hui-register-struct-at-point ()
   "Make a Hyperbole link to button register struct for button at point."
   (let* ((ebut-label (ebut:label-p))
          (ibut-label (ibut:label-p))
          (label (or ebut-label ibut-label)))
     (unless label
       (hypb:error "Point must be at a Hyperbole button"))
-    (make-hypb-register-but
+    (make-hui-register-but
      :label label
      :file (buffer-file-name)
      :mpos (point-marker)
@@ -48,26 +48,26 @@
 ;;; Private functions
 ;;; ************************************************************************
 
-(cl-defmethod register-val-jump-to ((val hypb-register-but) _arg)
+(cl-defmethod register-val-jump-to ((val hui-register-but) _arg)
   "Move point to location for Hyperbole button stored in VAL."
-  (let ((buf (marker-buffer (hypb-register-but-mpos val)))
-        (pos (marker-position (hypb-register-but-mpos val))))
+  (let ((buf (marker-buffer (hui-register-but-mpos val)))
+        (pos (marker-position (hui-register-but-mpos val))))
     (switch-to-buffer buf)
     (goto-char pos)))
 
-(cl-defmethod register-val-describe ((val hypb-register-but) _verbose)
+(cl-defmethod register-val-describe ((val hui-register-but) _verbose)
   "Print description of Hyperbole button register value VAL to `standard-output'."
   (princ "Hyperbole button\n    ")
   (princ (format "%s in file %s\n"
-                 (hypb-register-but-label val)
-                 (hypb-register-but-file val))))
+                 (hui-register-but-label val)
+                 (hui-register-but-file val))))
 
-(cl-defmethod register-val-insert ((val hypb-register-but))
+(cl-defmethod register-val-insert ((val hui-register-but))
   "Insert an ebut linking to the register button stored in VAL."
-  (ebut:program (hypb-register-but-label val)
-                (hypb-register-but-link val)
-                (hypb-register-but-label val)
-                (hypb-register-but-file val)))
+  (ebut:program (hui-register-but-label val)
+                (hui-register-but-link val)
+                (hui-register-but-label val)
+                (hui-register-but-file val)))
 
-(provide 'hypb-register)
-;;; hypb-register.el ends here
+(provide 'hui-register)
+;;; hui-register.el ends here
