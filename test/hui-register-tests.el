06a1d0ab0c4b01c44e878ca3180435efcf663ba0
Mats Lidell <mats.lidell@lidells.se>
2022-09-14 22:14:10 +0200

    Use name hui-register

--- test/hui-register-tests.el
+++ test/hui-register-tests.el
@@ -1,9 +1,9 @@
-;;; hypb-register-tests.el --- test for hypb-register         -*- lexical-binding: t; -*-
+;;; hui-register-tests.el --- test for hui-register         -*- lexical-binding: t; -*-
 ;;
 ;; Author:       Mats Lidell <matsl@gnu.org>
 ;;
 ;; Orig-Date:    10-Sep-22 at 20:43:17
-;; Last-Mod:     11-Sep-22 at 17:27:34 by Mats Lidell
+;; Last-Mod:     14-Sep-22 at 22:17:43 by Mats Lidell
 ;;
 ;; Copyright (C) 2021-2022  Free Software Foundation, Inc.
 ;; See the "HY-COPY" file for license information.
@@ -17,31 +17,31 @@
 
 (require 'ert)
 (require 'hmouse-drv)
-(require 'hypb-register)
+(require 'hui-register)
 
-(ert-deftest hypb-register-test--create-register-content ()
+(ert-deftest hui-register-test--create-register-content ()
   "Verify the struct contains its parts."
   (let ((file (make-temp-file "hypb")))
     (unwind-protect
         (find-file file)
         (insert "<[label]> $HOME")
         (goto-char 5)
-        (let ((content (hypb-register-struct-at-point)))
-          (should (equal (hypb-register-but-label content) "label"))
-          (should (equal (hypb-register-but-link content) 'link-to-ibut))
-          (should (markerp (hypb-register-but-mpos content)))
-          (should (equal (marker-buffer (hypb-register-but-mpos content)) (current-buffer)))
-          (should (equal (hypb-register-but-file content) (buffer-file-name)))))
+        (let ((content (hui-register-struct-at-point)))
+          (should (equal (hui-register-but-label content) "label"))
+          (should (equal (hui-register-but-link content) 'link-to-ibut))
+          (should (markerp (hui-register-but-mpos content)))
+          (should (equal (marker-buffer (hui-register-but-mpos content)) (current-buffer)))
+          (should (equal (hui-register-but-file content) (buffer-file-name)))))
     (delete-file file)))
 
-(ert-deftest hypb-register-test--register-val-jump-to ()
+(ert-deftest hui-register-test--register-val-jump-to ()
   "Verify register val jumps to right file."
   (let ((file (make-temp-file "hypb")))
     (unwind-protect
         (find-file file)
         (insert "<[label]> $HOME")
         (goto-char 5)
-        (let ((content (hypb-register-struct-at-point))
+        (let ((content (hui-register-struct-at-point))
               (pos (point)))
           (set-buffer "*scratch*")
           (should (equal (buffer-name) "*scratch*"))
@@ -50,7 +50,7 @@
           (should (equal pos (point)))))
     (delete-file file)))
 
-(ert-deftest hypb-register-test--register-val-insert ()
+(ert-deftest hui-register-test--register-val-insert ()
   "Verify register val inserts link to button."
   (let ((file1 (make-temp-file "hypb"))
         (file2 (make-temp-file "hypb")))
@@ -58,7 +58,7 @@
         (find-file file1)
         (insert "<[label]> $HOME")
         (goto-char 5)
-        (let ((content (hypb-register-struct-at-point))
+        (let ((content (hui-register-struct-at-point))
               (pos (point)))
           (find-file file2)
           (register-val-insert content)
@@ -70,5 +70,5 @@
     (delete-file file1)
     (delete-file file2)))
 
-(provide 'hypb-register-tests)
-;;; hypb-register-tests.el ends here
+(provide 'hui-register-tests)
+;;; hui-register-tests.el ends here
