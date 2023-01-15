05e933f4dcc56433007ff4f21ec2ca64dd52b539
Mats Lidell <mats.lidell@lidells.se>
2023-01-15 17:09:07 +0100

    Just call the old function, don't check if it is defined

--- hmouse-drv.el
+++ hmouse-drv.el
@@ -3,7 +3,7 @@
 ;; Author:       Bob Weiner
 ;;
 ;; Orig-Date:    04-Feb-90
-;; Last-Mod:     10-Dec-22 at 00:50:22 by Mats Lidell
+;; Last-Mod:     15-Jan-23 at 17:08:53 by Mats Lidell
 ;;
 ;; Copyright (C) 1989-2021  Free Software Foundation, Inc.
 ;; See the "HY-COPY" file for license information.
@@ -866,7 +866,7 @@ frame instead."
       ;; Drag frame when the window is on the bottom of its frame.
       (if (fboundp 'mouse-drag-frame-move) ;; From Emacs 28
           (mouse-drag-frame-move start-event)
-        (and (fboundp 'mouse-drag-frame) (mouse-drag-frame start-event 'move)))))))
+        (mouse-drag-frame start-event 'move))))))
 
 (defun hkey-debug (pred pred-value hkey-action)
   (message "(HyDebug) %sContext: %s; %s: %s; Buf: %s; Mode: %s; MinibufDepth: %s"
