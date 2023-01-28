0f6f41fdde9f5838965045ddf2aa5794b116808f
Mats Lidell <mats.lidell@lidells.se>
2023-01-28 23:29:36 +0100

    Require man for Man-notify-method

--- hactypes.el
+++ hactypes.el
@@ -3,7 +3,7 @@
 ;; Author:       Bob Weiner
 ;;
 ;; Orig-Date:    23-Sep-91 at 20:34:36
-;; Last-Mod:      7-Jan-23 at 19:59:13 by Bob Weiner
+;; Last-Mod:     28-Jan-23 at 23:27:48 by Mats Lidell
 ;;
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;;
@@ -18,7 +18,7 @@
 ;;; Other required Elisp libraries
 ;;; ************************************************************************
 
-(eval-and-compile (mapc #'require '(bookmark hvar hsettings comint hbut hpath hargs hmail)))
+(eval-and-compile (mapc #'require '(bookmark hvar hsettings comint hbut hpath hargs hmail man)))
 
 ;;; ************************************************************************
 ;;; Public declarations
