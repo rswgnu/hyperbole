871391f36840ba144407bbfd0d1f00361593c426
Mats Lidell <mats.lidell@lidells.se>
2024-01-20 19:46:32 +0100

    Add test for hargs:sexpression-p (#451)
    * Add test for hargs:sexpression-p
    
    * Fix hargs:sexpression-p when eval triggers an error
    
    Add test hargs-tests--sexpression-p

--- test/hargs-tests.el
+++ test/hargs-tests.el
@@ -3,7 +3,7 @@
 ;; Author:       Mats Lidell <matsl@gnu.org>
 ;;
 ;; Orig-Date:    04-Feb-22 at 23:00:00
-;; Last-Mod:     28-May-23 at 23:14:18 by Mats Lidell
+;; Last-Mod:     20-Jan-24 at 19:38:11 by Mats Lidell
 ;;
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;;
@@ -46,6 +46,26 @@
   "Verify hyperbole extension character +K is indentified."
   (cl-letf (((symbol-function 'hargs:read) (lambda (prompt &optional a b c d) "xyz")))
     (should (string= (hargs:get "+K: ") "xyz"))))
+
+(ert-deftest hargs-tests--sexpression-p ()
+  "Verify behavior of `hargs:sexpression-p'."
+  (with-temp-buffer
+    (insert " (setq var (+ 1 2))  ")
+    ;; pos ->123456789012345678901
+    (dolist (v '((1 nil nil)
+                 (2 "(setq var (+ 1 2))" "(setq var (+ 1 2))")
+                 (3 "(setq var (+ 1 2))" nil)
+                 (4 "(setq var (+ 1 2))" nil)
+                 (11 "(setq var (+ 1 2))" nil)
+                 (12 "(+ 1 2)" "(+ 1 2)")
+                 (13 "(+ 1 2)" nil)
+                 (18 "(+ 1 2)" nil)
+                 (19 "(+ 1 2)" "(+ 1 2)")
+                 (20 "(setq var (+ 1 2))" "(setq var (+ 1 2))")
+                 (21 nil nil)))
+      (goto-char (car v))
+      (should (string= (cadr v) (hargs:sexpression-p)))
+      (should (string= (caddr v) (hargs:sexpression-p t))))))
 
 ;; This file can't be byte-compiled without `with-simulated-input' which
 ;; is not part of the actual dependencies, so:
