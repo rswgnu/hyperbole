9057ea0a8a727b4cc28f94bdae3f39e7062995ee
bw <rsw@gnu.org>
2024-06-22 22:46:15 -0400

    hyrolo-org-mode - Fix #2 Org 9.7 and up breakage under Emacs 30

--- hyrolo.el
+++ hyrolo.el
@@ -3136,17 +3136,19 @@ Any non-nil value returned is a cons of (<entry-name> . <entry-source>)."
 
   (when (featurep 'org-fold) ;; newer Org versions
     (setq org-fold-core-style 'overlays) ;; Make compatible with reveal minor mode
-    (when (and org-link-descriptive
-               (eq org-fold-core-style 'overlays))
-      (hypb:add-to-invisibility-spec '(org-link)))
+    (hypb:add-to-invisibility-spec '(org-link))
     (org-fold-initialize (or (and (stringp org-ellipsis) (not (equal "" org-ellipsis)) org-ellipsis)
                              "..."))
     (make-local-variable 'org-link-descriptive)
     (when (eq org-fold-core-style 'overlays)
       (hypb:add-to-invisibility-spec '(org-hide-block . t)))
-    (if org-link-descriptive
-	(org-fold-core-set-folding-spec-property 'org-link :visible nil)
-      (org-fold-core-set-folding-spec-property 'org-link :visible t)))
+    (org-fold-core-set-folding-spec-property
+     (if (boundp 'org-link--link-folding-spec)
+	 ;; Org pre-9.7
+	 (car org-link--link-folding-spec)
+       ;; Org 9.7 and up
+       (caar org-fold-core--specs))
+     :visible (not org-link-descriptive)))
 
   (setq-local hyrolo-entry-regexp "^\\(\\*+\\)\\([ \t\n\r]+\\)"
 	      hyrolo-hdr-and-entry-regexp (concat hyrolo-hdr-prefix-regexp hyrolo-entry-regexp)
