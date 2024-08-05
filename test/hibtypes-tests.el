92e7c3073282834acdae7e741b5024365965b9fa
Mats Lidell <mats.lidell@lidells.se>
2024-08-05 17:38:28 +0200

    Use cl-letf to mock buffer-name

--- test/hibtypes-tests.el
+++ test/hibtypes-tests.el
@@ -3,7 +3,7 @@
 ;; Author:       Mats Lidell <matsl@gnu.org>
 ;;
 ;; Orig-Date:    20-Feb-21 at 23:45:00
-;; Last-Mod:      4-Aug-24 at 23:34:43 by Mats Lidell
+;; Last-Mod:      5-Aug-24 at 17:37:57 by Mats Lidell
 ;;
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;;
@@ -307,31 +307,33 @@
 ;; elisp-compiler-msg
 (ert-deftest elisp-compiler-msg-test ()
   "Verify elisp-compiler-msg."
-  (with-temp-buffer
-    (insert "    passed  1/1  test (0.000100 sec)\n")
-    (goto-line 1)
-    (mocklet (((buffer-name) => "*Compile-Log*")
-              ((smart-tags-display "test" nil) => t))
-      (should (ibtypes::elisp-compiler-msg))))
+  (let ((orig-buffer-name (symbol-function 'buffer-name)))
+    (cl-letf (((symbol-function 'buffer-name)
+               (lambda (&optional buffer)
+                 (if (string-prefix-p " *temp*" (funcall orig-buffer-name))
+                     "*Compile-Log*"
+                   (funcall orig-buffer-name)))))
+      (with-temp-buffer
+        (insert "    passed  1/1  abcde (0.000100 sec)\n")
+        (goto-line 1)
+        (mocklet (((smart-tags-display "abcde" nil) => t))
+          (should (ibtypes::elisp-compiler-msg))))
 
-  (with-temp-buffer
-    (insert "Test test-test backtrace:\n")
-    (goto-line 1)
-    (mocklet (((buffer-name) => "*Compile-Log*")
-              ((smart-tags-display "test-test" nil) => t))
-      (should (ibtypes::elisp-compiler-msg))))
+      (with-temp-buffer
+        (insert "Test abcde backtrace:\n")
+        (goto-line 1)
+        (mocklet (((smart-tags-display "abcde" nil) => t))
+          (should (ibtypes::elisp-compiler-msg))))
 
-  (with-temp-buffer
-    (insert "Compiling /home/user/file.el...
+      (with-temp-buffer
+        (insert "Compiling /home/user/file.el...
 
 In hyperbole-test:
 file.el:10:20: Warning: Message
 ")
-    (goto-line 3)
-    (mocklet (((buffer-name) => "*Compile-Log*")
-              ((actypes::link-to-regexp-match "^(def[a-z \11]+hyperbole-test[ \11\n\15(]" 1 "/home/user/file.el" nil) => t))
-      (should (ibtypes::elisp-compiler-msg))))
-  )
+        (goto-line 3)
+        (mocklet (((actypes::link-to-regexp-match "^(def[a-z \11]+hyperbole-test[ \11\n\15(]" 1 "/home/user/file.el" nil) => t))
+          (should (ibtypes::elisp-compiler-msg)))))))
 
 ;; patch-msg
 
