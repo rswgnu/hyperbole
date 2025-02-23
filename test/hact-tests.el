40f5f900387606e6be7fc8ca41d05858f96ed20f
bw <rsw@gnu.org>
2025-02-23 10:24:01 -0500

    hycontrol.el - Eliminate error if frame-cmds library is missing

--- test/hact-tests.el
+++ test/hact-tests.el
@@ -3,7 +3,7 @@
 ;; Author:       Mats Lidell
 ;;
 ;; Orig-Date:    16-May-24 at 00:29:22
-;; Last-Mod:     16-May-24 at 23:51:18 by Mats Lidell
+;; Last-Mod:     22-Feb-25 at 09:35:56 by Bob Weiner
 ;;
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;;
@@ -23,8 +23,8 @@
 (ert-deftest hact-tests--action-params-with-lambdas ()
   "Lambda used with `action:params' should return the lambda parameters."
   (should (equal nil (action:params (lambda () nil))))
-  (should (equal '(x) (action:params (lambda (x) nil))))
-  (should (equal '(x y) (action:params (lambda (x y) nil)))))
+  (should (equal '(_x) (action:params (lambda (_x) nil))))
+  (should (equal '(_x _y) (action:params (lambda (_x _y) nil)))))
 
 (ert-deftest hact-tests--actype-act-with-lambdas ()
   "Lambda with `actype:act' should work over versions of Emacs.
