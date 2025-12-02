ef01148d27848777dfce8d2baadca3cacf073212
Mats Lidell <mats.lidell@lidells.se>
2025-12-02 12:38:23 +0100

    Generate manual for getting the published date right (#826)

--- man/.dir-locals.el
+++ man/.dir-locals.el
@@ -2,13 +2,13 @@
   . ((before-save-hook
       . (lambda ()
           (with-locale-environment "en_US.utf8"
-            (let ((day (format-time-string "%d" (current-time)))
+            (let ((day (string-trim (format-time-string "%e" (current-time))))
                   (month (capitalize (format-time-string "%B" (current-time))))
                   (year (format-time-string "%Y" (current-time))))
               (save-excursion
                 (goto-char (point-min))
-                (when (re-search-forward "\\(@set UPDATED [[:word:]]+, [[:digit:]]+\\)" nil t)
-                  (replace-match (format "@set UPDATED %s, %s" month year) nil t nil nil))
+                (when (re-search-forward "\\(@set UPDATED [[:word:]]+ [[:digit:]]+, [[:digit:]]+\\)" nil t)
+                  (replace-match (format "@set UPDATED %s %s, %s" month day year) nil t nil nil))
                 (goto-char (point-min))
                 (when (re-search-forward "\\(@set UPDATED-MONTH [[:word:]]+ [[:digit:]]+\\)" nil t)
                   (replace-match (format "@set UPDATED-MONTH %s %s" month year) nil t nil nil))
