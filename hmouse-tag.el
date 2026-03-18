4ad4320cf6b8d0dc5350a3bbbc395a528f9c001f
Mats Lidell <mats.lidell@lidells.se>
2026-03-19 00:34:35 +0100

    Use find-library, defined since Emacs 25.2
    * hmouse-tag.el (smart-lisp):
    (smart-lisp-at-known-identifier-p): Use find-library, defined since Emacs 25.2.

--- hmouse-tag.el
+++ hmouse-tag.el
@@ -667,36 +667,36 @@ pressed in an appropriate buffer and has moved the cursor to the selected
 buffer."
 
   (interactive)
-  (unless (and (fboundp 'find-library)
-	       ;; Handle Emacs Lisp `require', `load', and `autoload' clauses.
-	       (let ((opoint (point))
-		     type
-		     name
-		     lib)
-		 (setq lib (and (search-backward "\(" nil t)
-				(or
-				 ;; load with a filename
-				 (looking-at "(\\(load\\)[ \t]+\\(\\)\"\\([^][() \t\n\r`'\"]+\\)")
-				 ;; autoload or require with a name and filename
-				 (looking-at "(\\(autoload\\|require\\)[ \t]+'\\([^][() \t\n\r`'\"]+\\)[ \t\n\r]+\"\\([^][() \t\n\r`'\"]+\\)")
-				 ;; require without a separate filename
-				 (looking-at "(\\(require\\)\\(\\)[ \t]+'\\([^][() \t\n\r`'\"]+\\)"))))
-		 (goto-char opoint)
-		 (when lib
-		   (setq type (match-string-no-properties 1)
-			 name (match-string-no-properties 2)
-			 lib (match-string-no-properties 3))
-		   (hpath:display-buffer (current-buffer))
-		   (find-library lib)
-		   (goto-char (point-min))
-		   (when (equal type "autoload")
-		     ;; Ignore defgroup matches
-		     (if (re-search-forward
-			  (format "^[; \t]*(def.[^r][^ \t\n\r]*[ \t]+%s[ \t\n\r]" (regexp-quote name))
-			  nil t)
-			 (goto-char (match-beginning 0))
-		       (error "(smart-lisp): Found autoload library but no definition for `%s'" name)))
-		   t)))
+  (unless
+      ;; Handle Emacs Lisp `require', `load', and `autoload' clauses.
+      (let ((opoint (point))
+	    type
+	    name
+	    lib)
+	(setq lib (and (search-backward "\(" nil t)
+		       (or
+			;; load with a filename
+			(looking-at "(\\(load\\)[ \t]+\\(\\)\"\\([^][() \t\n\r`'\"]+\\)")
+			;; autoload or require with a name and filename
+			(looking-at "(\\(autoload\\|require\\)[ \t]+'\\([^][() \t\n\r`'\"]+\\)[ \t\n\r]+\"\\([^][() \t\n\r`'\"]+\\)")
+			;; require without a separate filename
+			(looking-at "(\\(require\\)\\(\\)[ \t]+'\\([^][() \t\n\r`'\"]+\\)"))))
+	(goto-char opoint)
+	(when lib
+	  (setq type (match-string-no-properties 1)
+		name (match-string-no-properties 2)
+		lib (match-string-no-properties 3))
+	  (hpath:display-buffer (current-buffer))
+	  (find-library lib)
+	  (goto-char (point-min))
+	  (when (equal type "autoload")
+	    ;; Ignore defgroup matches
+	    (if (re-search-forward
+		 (format "^[; \t]*(def.[^r][^ \t\n\r]*[ \t]+%s[ \t\n\r]" (regexp-quote name))
+		 nil t)
+		(goto-char (match-beginning 0))
+	      (error "(smart-lisp): Found autoload library but no definition for `%s'" name)))
+	  t))
     (smart-lisp-find-tag nil show-doc)))
 
 (defun smart-lisp-find-tag (&optional tag show-doc)
@@ -1256,18 +1256,18 @@ This indicates that TAG is serving as a hyperlink button."
 The Lisp identifier is either listed in a tags table or is a
 known Emacs Lisp identifier."
   (interactive)
-  (unless (and (fboundp 'find-library)
-	       ;; Handle Emacs Lisp `require', `load', and `autoload' clauses.
-	       (let ((lib)
-		     (opoint (point)))
-		 (setq lib (and (re-search-backward "[()]" nil t)
-				(looking-at (concat
-					     "(\\(require\\|load\\|autoload\\)"
-					     "[ \t]+.*['\"]"
-					     "\\([^][() \t\n\r`'\"]+\\)"))))
-		 (goto-char opoint)
-		 (when lib
-		   (ignore-errors (and (find-library-name lib) t)))))
+  (unless
+      ;; Handle Emacs Lisp `require', `load', and `autoload' clauses.
+      (let ((lib)
+	    (opoint (point)))
+	(setq lib (and (re-search-backward "[()]" nil t)
+		       (looking-at (concat
+				    "(\\(require\\|load\\|autoload\\)"
+				    "[ \t]+.*['\"]"
+				    "\\([^][() \t\n\r`'\"]+\\)"))))
+	(goto-char opoint)
+	(when lib
+	  (ignore-errors (and (find-library-name lib) t))))
     ;; Cache tag value
     (setq hkey-value (smart-lisp-at-tag-p t))
     (let* ((tag hkey-value)
