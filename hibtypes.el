3f4f76e4f0376a3a7fd0e50e5b27e969d0cb2419
Mats Lidell <mats.lidell@lidells.se>
2026-03-19 00:02:59 +0100

    Use get-text-property and gnus-article-press-button
    Defined at least since Emacs 28.
    
    Text property 'gnus-callback ensures gnus is loaded.
    
    * hibtypes.el (gnus-push-button): Use get-text-property and
    gnus-article-press-button.

--- hibtypes.el
+++ hibtypes.el
@@ -1523,9 +1523,7 @@ documentation string is displayed."
 (defib gnus-push-button ()
   "Activate GNUS-specific article push-buttons, e.g. for hiding signatures.
 GNUS is a news and mail reader."
-  (and (fboundp 'get-text-property)
-       (fboundp 'gnus-article-press-button)
-       (get-text-property (point) 'gnus-callback)
+  (and (get-text-property (point) 'gnus-callback)
        (let* ((but (button-at (point)))
 	      (but-start (when but (button-start but)))
 	      (but-end (when but (button-end but))))
