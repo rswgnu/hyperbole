ed9ccb2f0383615d5e3d3d5ff990fca9e1e339f5
Mats Lidell <mats.lidell@lidells.se>
2026-06-24 00:40:11 +0200

    More tests

--- test/hasht-tests.el
+++ test/hasht-tests.el
@@ -3,7 +3,7 @@
 ;; Author:       Mats Lidell
 ;;
 ;; Orig-Date:    22-Jun-26 at 23:25:12
-;; Last-Mod:     23-Jun-26 at 00:49:42 by Mats Lidell
+;; Last-Mod:     24-Jun-26 at 00:24:45 by Mats Lidell
 ;;
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;;
@@ -18,6 +18,8 @@
 ;;; Code:
 
 (require 'ert)
+(require 'ert-x)
+(require 'hy-test-helpers)
 (require 'hasht)
 
 (ert-deftest hasht-tests--make ()
@@ -28,17 +30,108 @@
     (should (hash-empty-p h1))
     (should (= 1 (hash-table-size h1)))
     (should (= 10 (hash-table-size h10))))
-  (should (string= "val" (hash-lookup "key" (hash-make '(("val" . "key"))))))
-  (should (string= "val2" (hash-lookup "key" (hash-make '(("val" . "key") ("val2" . "key")))))))
+  (should (= 1 (hash-lookup "k" (hash-make '((1 . "k"))))))
+  (should (= 2 (hash-lookup "k" (hash-make '((1 . "k") (2 . "k"))))))
+  ;; Reverse
+  (should (= 2 (hash-lookup "k" (hash-make '(("k" . 1) ("k" . 2)) t)))))
+
+(ert-deftest hasht-tests--make-prepend ()
+  "Verify `hash-make-prepend'."
+  (should (equal '(1) (hash-lookup "k1" (hash-make-prepend '((1 . "k1"))))))
+  (should (equal '(2 1) (hash-lookup "k1" (hash-make-prepend '((1 . "k1") (2 . "k1"))))))
+  ;; Reverse
+  (should (equal '(1) (hash-lookup "k1" (hash-make-prepend '(("k1" . 1)) t)))))
+
+(ert-deftest hasht-tests--add ()
+  "Verify `hash-add'."
+  (let ((h (hash-make nil)))
+    ;; Add one
+    (should (= 1 (hash-add 1 "k" h)))
+    (should (= 1 (hash-count h)))
+    ;; Add with same key should replace
+    (should (= 2 (hash-add 2 "k" h)))
+    (should (= 1 (hash-count h)))
+    ;; And one more
+    (should (= 2 (hash-add 2 "k2" h)))
+    (should (= 2 (hash-count h)))
+    ;; Add value nil for key removes it
+    (should-not (hash-add nil "k" h))
+    (should-not (hash-key-p "k" h))))
+
+(ert-deftest hasht-tests--map ()
+  "Verify `hash-map´."
+  (let ((h (hash-make '((1 . "k1") (2 . "k2")))))
+    (should (equal '((1 . "k1") (2 . "k2")) (hash-map #'identity h)))
+    (should (equal '(2 1) (hash-map #'car h)))
+    (should (equal '("k2" "k1") (hash-map #'cdr h)))))
+
+(ert-deftest hasht-tests--copy ()
+  "Verify `hash-copy'."
+  :expected-result :failed
+  (let* ((h (hash-make '((1 . "k"))))
+         (copy (hash-copy h)))
+    (should (= 1 (hash-lookup "k" h)))
+    (should (= 1 (hash-lookup "k" copy)))))
+
+(ert-deftest hasht-tests--deep-copy ()
+  "Verify `hash-deep-copy'."
+  :expected-result :failed
+  (let* ((h (hash-make '((1 . "k"))))
+         (copy (hash-deep-copy h)))
+    (should (= 1 (hash-lookup "k" h)))
+    (should (= 1 (hash-lookup "k" copy)))))
+
+(ert-deftest hasht-tests--delete ()
+  "Verify `hash-delete'."
+  (let ((h (hash-make '((1 . "k1")))))
+    (should (= 1 (hash-count h)))
+    (should (hash-delete "k1" h))
+    (should (= 0 (hash-count h)))
+    (should-not (hash-delete "k1" h))
+    (should-not (hash-delete "k_other" h))))
+
+(ert-deftest hasht-tests--key-p ()
+  "Verify `hash-key-p'."
+  (let ((h (hash-make '((1 . "k")))))
+    (should (hash-key-p "k" h))
+    (should (= 1 (hash-lookup "k" h)))
+    (should-not (hash-key-p "1" h))))
 
 (ert-deftest hasht-tests--merge ()
   "Verify `hash-merge'."
-  (let ((h1 (hash-make '(("val" . "key"))))
-        (h2 (hash-make '(("val2" . "key2")))))
-    (should (string= "val" (hash-lookup "key" h1)))
-    (should (string= "val" (hash-lookup "key" (hash-merge h1))))
-    (let ((m (hash-merge h1 h2)))
-      (should (string= "val" (hash-lookup "key" m))))))
+  (let ((h (hash-make '((1 . "k")))))
+    (should (= 1 (hash-lookup "k" (hash-merge h))))
+    (should (equal '(1 1) (hash-lookup "k" (hash-merge h h))))))
+
+(ert-deftest hasht-tests--merge-list-val ()
+  "Verify `hash-merge' with list value."
+  :expected-result :failed
+  (let ((h (hash-make '(((1 2) . "k")))))
+    (should (equal '(1 2) (hash-lookup "k" (hash-merge h h))))))
+
+(ert-deftest hasht-tests--prepend ()
+  "Verify `hash-prepend'."
+  (let ((h (hash-make '((1 . "k")))))
+    (should-error (hash-prepend 2 "k" h))
+    (should (hash-add '(1) "k" h))
+    (should (equal '(2 1) (hash-prepend 2 "k" h)))))
+
+(ert-deftest hasht-tests--prin1 ()
+  "Verify `hash-prin1'."
+  (ert-with-message-capture cap
+    (hash-prin1 (hash-make '((1 . "k"))))
+    (hy-test-helpers:should-last-message "(\n(1 . \"k\")\n)\n" cap)))
+
+(ert-deftest hasht-tests--replace ()
+  "Verify `hash-replace'."
+  (let ((h (hash-make '((1 . "k")))))
+    (should (= 2 (hash-replace 2 "k" h)))
+    (should (= 2 (hash-lookup "k" h)))))
+
+(ert-deftest hasht-tests--size ()
+  "Verify `hash-size'."
+  (should (<= 1 (hash-size (hash-make 1))))
+  (should (<= 10 (hash-size (hash-make 10)))))
 
 (provide 'hasht-tests)
 ;;; hasht-tests.el ends here
